2e053d389227a946c474a871b2e155b3ad4f3984
Aryan Donga <daryan6680@proton.me>
2023-12-16 19:27:27 +0530

    Shift to repotracking features

--- script.js
+++ script.js
@@ -1,9 +1,30 @@
-async function fetchData() {
-    const res=await fetch ("https://api.coronavirus.data.gov.uk/v1/data");
-    const record=await res.json();
-    document.getElementById("date").innerHTML=record.data[0].date;
-    document.getElementById("areaName").innerHTML=record.data[0].areaName;
-    document.getElementById("latestBy").innerHTML=record.data[0].latestBy;
-    document.getElementById("deathNew").innerHTML=record.data[0].deathNew;
+const { Octokit, App } = require("octokit");
+require('dotenv').config();
+
+const config = require('./config.json');
+
+// document.getElementById("id").innerHTML
+// https://github.com/MCUxDaredevil/repotracker-extension/commit/d092a75161f08cffd9b59f7ff0cd239e4e92cc54
+// Color dim red if archived
+// Color dim green if new update or no past data in database
+
+async function main() {
+    const api = new Octokit({auth: process.env.GITHUB_TOKEN});
+    const repositories = config.repo_list;
+    for (const repository of repositories) {
+        const [owner, repo] = repository.split("github.com/")[1].split("/");
+
+        await api.request(`GET /repos/${owner}/${repo}/`, {
+            headers: {
+                accept: "application/vnd.github+json"
+            },
+        }).then((response) => {
+            const data = response.data;
+            console.log(data.pushed_at);
+        }).catch((error) => {
+            console.log(error);
+        });
+    }
 }
-fetchData();
+
+main();
